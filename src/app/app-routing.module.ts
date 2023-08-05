52bb33e4b937bf7534acf34775e3be8b0c11cd0b
vanshika5719 <136620935+vanshika5719@users.noreply.github.com>
2023-08-05 19:31:28 +0530

    separated the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
 
-const routes: Routes = [ { path: "sign-up", component: SignUpComponent },];
+const routes: Routes = [   
+{ path: "", redirectTo: "/", pathMatch: "full" },
+{ path: "", component: LoginPageComponent },
+{ path: "sign-up", component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
-
-}
+export class AppRoutingModule {}
