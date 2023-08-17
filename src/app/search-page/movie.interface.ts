4f8f9bae9ead20e8a8b7fd4815e47d1e66ce70be
vanshika-05 <66822404+vanshika-05@users.noreply.github.com>
2023-08-17 13:23:00 +0100

    added trend/recom list -vanshika

---
+++ src/app/search-page/movie.interface.ts
@@ -0,0 +1,19 @@
+export interface movie {
+
+    Cast: string,
+    Detail: string,
+    Disney: number,
+    Genre: string,
+    HBO: number,
+    IsTrending: string,
+    MovieID: string,
+    MovieName: string,
+    Netflix: number,
+    Poster: string,
+    Prime: number,
+    Rating: string,
+    ReleaseDate: string,
+    createdAt: string,
+    id: number,
+    updatedAt: string,
+    }
--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,6 +1,7 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ScreenhuntService } from '../services/screenhunt.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { movie } from './movie.interface';
 
 
 @Component({
@@ -11,88 +12,103 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 export class SearchPageComponent implements OnInit {
 
-  allMovies: any;
-  
-  constructor(private screenhuntService: ScreenhuntService,
-    private route:ActivatedRoute,
-    private router:Router) { }
+  allMovies: movie[]=[];
+  recommendedMovies: movie[]=[];
+  trendingMovies: movie[]=[];
 
-  ngOnInit(): void { this.retrieveAllMovies();
-   }
-   retrieveAllMovies():void {
-    // this.Isloading=true;
+  constructor(private screenhuntService: ScreenhuntService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.retrieveAllMovies();
+  }
+  retrieveAllMovies(): void {
     this.screenhuntService.getAll()
       .subscribe(
         data => {
           this.allMovies = data;
-          console.log(data);
-          // this.Isloading=false;
+          console.log('All Movies:', this.allMovies);
+          // this.recommendedMovies = [];
+          // this.trendingMovies = [];
+          this.allMovies.forEach((movie) => {
+            // console.log('Movie:', movie);
+            if (movie.Genre == 'Drama') {
+              this.recommendedMovies.push(movie);
+            }
+            if (movie.IsTrending == '1') {
+              this.trendingMovies.push(movie);
+            }
+          });
+          console.log('Recommended Movies:', this.recommendedMovies);
+          console.log('Trending Movies:', this.trendingMovies);
         },
         error => {
           console.log(error);
-
         });
   }
 
-  toSection3(){
-    let x= document.querySelector("#section3");
-    if(x){
+
+
+  toSection3() {
+    let x = document.querySelector("#section3");
+    if (x) {
       x.scrollIntoView({
-        behavior:'smooth',
+        behavior: 'smooth',
       }
       )
     }
   }
 
-  toSection2(){
-    let x= document.querySelector("#section2");
-    if(x){
+  toSection2() {
+    let x = document.querySelector("#section2");
+    if (x) {
       x.scrollIntoView({
-        behavior:'smooth',
+        behavior: 'smooth',
       }
       )
     }
   }
 
-  toSection1(){
-    let x= document.querySelector("#section1");
-    if(x){
+  toSection1() {
+    let x = document.querySelector("#section1");
+    if (x) {
       x.scrollIntoView({
-        behavior:'smooth',
+        behavior: 'smooth',
       }
       )
     }
   }
 
 
-toSection6(){
-  let x= document.querySelector("#section6");
-  if(x){
-    x.scrollIntoView({
-      behavior:'smooth',
+  toSection6() {
+    let x = document.querySelector("#section6");
+    if (x) {
+      x.scrollIntoView({
+        behavior: 'smooth',
+      }
+      )
     }
-    )
   }
-}
 
-toSection5(){
-  let x= document.querySelector("#section5");
-  if(x){
-    x.scrollIntoView({
-      behavior:'smooth',
+  toSection5() {
+    let x = document.querySelector("#section5");
+    if (x) {
+      x.scrollIntoView({
+        behavior: 'smooth',
+      }
+      )
     }
-    )
   }
-}
 
-toSection4(){
-  let x= document.querySelector("#section4");
-  if(x){
-    x.scrollIntoView({
-      behavior:'smooth',
+  toSection4() {
+    let x = document.querySelector("#section4");
+    if (x) {
+      x.scrollIntoView({
+        behavior: 'smooth',
+      }
+      )
     }
-    )
   }
-}
 
 }
