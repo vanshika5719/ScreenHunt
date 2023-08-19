ca2b6e5932d014241d86614cedfb7e655a879208
vanshika-05 <66822404+vanshika-05@users.noreply.github.com>
2023-08-19 09:30:20 +0100

    signup form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +23,7 @@ import { SearchResultComponent } from './search-result/search-result.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
 
+  constructor() {}
+  errmsg:any;
+  errmsgshow: boolean = false;
+
+  signupForm = new FormGroup({
+    fname: new FormControl('', Validators.required),
+    emailid: new FormControl('', Validators.required),
+    pass: new FormControl('', Validators.required),
+    lname: new FormControl('', Validators.required),
+    interest: new FormControl('', Validators.required),
+    subscriptions: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false)
+    ])
+  });
+
+
+
+  ngOnInit(): void {
+  }
+
+  signupSubmit()
+  {  
+    if(this.signupForm.valid)
+    {
+      console.log(this.signupForm.value, 'signuform###');
+      this.errmsgshow = false;
+    }
+    else
+    {
+      this.errmsgshow = true;
+      this.errmsg = "All Fields Required";
+    }
+      
+    }
 }
