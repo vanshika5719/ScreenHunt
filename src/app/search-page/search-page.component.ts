de66789e2be26ab6c3c26312677ce1a0985817ad
vanshika-05 <66822404+vanshika-05@users.noreply.github.com>
2023-08-29 17:42:15 +0100

    routing

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -103,7 +103,7 @@ export class SearchPageComponent implements OnInit {
 
   logout() {
     localStorage.clear(); 
-    this.router.navigate(['/']); 
+    this.router.navigate(['/login']); 
   }
 
   // toSection3() {
